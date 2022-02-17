d2a03130e7efa2d9cbf16874f0fd99ae0632d48a
Olivier van Helden <olivier@van-helden.net>
2022-02-16 21:33:21 -0400

    removed obsolete test code

--- admin/wc-admin-classes.php
+++ admin/wc-admin-classes.php
@@ -143,60 +143,6 @@ class WCDNR_Admin {
     return apply_filters( 'wcdnr_settings', $settings );
   }
 
-  public static function custom_fields()
-  {
-    global $woocommerce, $post;
-    echo '<div class="product_custom_field">';
-    // Custom Product Text Field
-    woocommerce_wp_text_input(
-      array(
-        'id' => '_custom_product_text_field',
-        'placeholder' => 'Custom Product Text Field',
-        'label' => __('Custom Product Text Field', 'woocommerce'),
-        'desc_tip' => 'true'
-      )
-    );
-    //Custom Product Number Field
-    woocommerce_wp_text_input(
-      array(
-        'id' => '_custom_product_number_field',
-        'placeholder' => 'Custom Product Number Field',
-        'label' => __('Custom Product Number Field', 'woocommerce'),
-        'type' => 'number',
-        'custom_attributes' => array(
-          'step' => 'any',
-          'min' => '0'
-        )
-      )
-    );
-    //Custom Product  Textarea
-    woocommerce_wp_textarea_input(
-      array(
-        'id' => '_custom_product_textarea',
-        'placeholder' => 'Custom Product Textarea',
-        'label' => __('Custom Product Textarea', 'woocommerce')
-      )
-    );
-    echo '</div>';
-  }
-
-  public static function custom_fields_save($post_id)
-  {
-    // Custom Product Text Field
-    $woocommerce_custom_product_text_field = $_POST['_custom_product_text_field'];
-    if (!empty($woocommerce_custom_product_text_field))
-    update_post_meta($post_id, '_custom_product_text_field', esc_attr($woocommerce_custom_product_text_field));
-    // Custom Product Number Field
-    $woocommerce_custom_product_number_field = $_POST['_custom_product_number_field'];
-    if (!empty($woocommerce_custom_product_number_field))
-    update_post_meta($post_id, '_custom_product_number_field', esc_attr($woocommerce_custom_product_number_field));
-    // Custom Product Textarea Field
-    $woocommerce_custom_procut_textarea = $_POST['_custom_product_textarea'];
-    if (!empty($woocommerce_custom_procut_textarea))
-    update_post_meta($post_id, '_custom_product_textarea', esc_html($woocommerce_custom_procut_textarea));
-
-  }
-
   public static function check_credentials($value, $option, $raw_value) {
     $cached = wp_cache_get('wcdnr_check_credentials', 'wcdnr');
     if($cached == 'success') return $value;
