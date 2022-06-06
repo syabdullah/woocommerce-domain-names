8cce8681258d914f89084b6b6d01092564751ee6
Olivier van Helden <olivier@van-helden.net>
2022-06-06 09:47:57 -0400

    composer

--- includes/classes.php
+++ includes/classes.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(__DIR__ . '/functions.php');
-require_once(__DIR__ . '/openprovider-api.php');
+// require_once(__DIR__ . '/openprovider-api.php');
 
 /**
  * [WCDNR description]
