8dfca2fce3099d9ea2e91cc3255b602b9bf808a5
Olivier van Helden <olivier@van-helden.net>
2023-01-16 13:12:22 -0400

    updated external libraries

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -49,8 +49,8 @@ class ComposerStaticInitffdae5c486c9494b5b14f8e735a2e095
     public static $prefixDirsPsr4 = array (
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
